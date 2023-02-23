7442fbebede5f1312f9efd4da7527c0ed281a123
dgaisan <dima.gaisan@gmail.com>
2023-02-22 17:05:21 -0700

    [todo-list-with-gql-dynamodb] creating IAM roles

--- todo-list-with-gql-dynamodb/lib/todo-list-with-gql-dynamodb-stack.ts
+++ todo-list-with-gql-dynamodb/lib/todo-list-with-gql-dynamodb-stack.ts
@@ -1,16 +1,48 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import {
+  ManagedPolicy,
+  Policy,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
+import { Construct } from "constructs";
 
 export class TodoListWithGqlDynamodbStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const loggingRole = new Role(this, "ToDoLoggingRole", {
+      assumedBy: new ServicePrincipal("dynamodb.amazonaws.com"),
+      managedPolicies: [
+        cdk.aws_iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSAppSyncPushToCloudWatchLogs"
+        ),
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoListWithGqlDynamodbQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const dataAccessRole = new Role(this, "ToDoDataAcessRole", {
+      assumedBy: new ServicePrincipal("dynamodb.amazonaws.com"),
+    });
+    dataAccessRole.attachInlinePolicy(
+      new Policy(this, "dynamodb-read-write", {
+        statements: [
+          new PolicyStatement({
+            actions: [
+              "dynamodb:BatchGetItem",
+              "dynamodb:BatchWriteItem",
+              "dynamodb:PutItem",
+              "dynamodb:DeleteItem",
+              "dynamodb:GetItem",
+              "dynamodb:Scan",
+              "dynamodb:Query",
+              "dynamodb:UpdateItem",
+            ],
+             resources: ["*"]
+          }),
+        ],
+      })
+    );
   }
 }
--- todo-list-with-gql-dynamodb/test/todo-list-with-gql-dynamodb.test.ts
+++ todo-list-with-gql-dynamodb/test/todo-list-with-gql-dynamodb.test.ts
@@ -1,17 +1,26 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as TodoListWithGqlDynamodb from '../lib/todo-list-with-gql-dynamodb-stack';
+import * as cdk from "aws-cdk-lib";
+import { Capture, Match, Template } from "aws-cdk-lib/assertions";
+import * as TodoListWithGqlDynamodb from "../lib/todo-list-with-gql-dynamodb-stack";
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/todo-list-with-gql-dynamodb-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new TodoListWithGqlDynamodb.TodoListWithGqlDynamodbStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test("GraphQL Api created", () => {
+  const app = new cdk.App();
+  const stack = new TodoListWithGqlDynamodb.TodoListWithGqlDynamodbStack(
+    app,
+    "MyGraphQL-With-DynamoDB-Stack"
+  );
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties("AWS::IAM::Role", {
+    AssumeRolePolicyDocument: {
+      Statement: [
+        {
+          Action: "sts:AssumeRole",
+          Effect: "Allow",
+          Principal: {
+            Service: "dynamodb.amazonaws.com",
+          },
+        },
+      ],
+    },
+  });
 });
