4f1ff67cd829466b9d40fd5479bb26f0ef2744f6
dgaisan <dima.gaisan@gmail.com>
2023-06-08 18:25:38 +0300

    cdk-cognito-auth0 initial commit

---
+++ cdk-cognito-auth0/lib/cdk-cognito-auth0-stack.ts
@@ -0,0 +1,12 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class CdkCognitoAuth0Stack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    
+  }
+}
---
+++ cdk-cognito-auth0/test/cdk-cognito-auth0.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkCognitoAuth0 from '../lib/cdk-cognito-auth0-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-cognito-auth0-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkCognitoAuth0.CdkCognitoAuth0Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
