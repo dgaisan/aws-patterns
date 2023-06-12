36e1023aa71deca88c43056179ab72b59d0c518f
dgaisan <dima.gaisan@gmail.com>
2023-06-12 23:50:04 +0300

    [cdk-cognito-auth0] Small updates Cognito stack

--- cdk-cognito-auth0/lib/stacks/cognito-stack.ts
+++ cdk-cognito-auth0/lib/stacks/cognito-stack.ts
@@ -9,6 +9,8 @@ import config from "./../config";
 
 export class CongnitoStack extends Stack {
   public readonly identityPool: cognito.CfnIdentityPool;
+  public readonly authenticatedUserGroupRole: iam.Role;
+  public readonly unauthenticatedUserGroupRole: iam.Role;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
@@ -25,6 +27,64 @@ export class CongnitoStack extends Stack {
         identityPoolName: "auth0-identity-pool",
         allowUnauthenticatedIdentities: true,
         openIdConnectProviderArns: [provider.openIdConnectProviderArn],
+      }
+    );
+
+    this.unauthenticatedUserGroupRole = new iam.Role(
+      this,
+      "auth0-unauth-group-role",
+      {
+        description: "role for unauthenticated users",
+        assumedBy: new iam.FederatedPrincipal(
+          "cognito-identity.amazonaws.com",
+          {
+            StringEquals: {
+              "cognito-identity.amazonaws.com:aud": this.identityPool.ref,
+            },
+            "ForAnyValue:StringLike": {
+              "cognito-identity.amazonaws.com:amr": "unauthenticated",
+            },
+          },
+          "sts:AssumeRoleWithWebIdentity"
+        ),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            "service-role/AWSLambdaBasicExecutionRole"
+          ),
+        ],
+      }
+    );
+
+    this.authenticatedUserGroupRole = new iam.Role(this, "auth0-auth-group-role", {
+      description: "role for authenticated users",
+      assumedBy: new iam.FederatedPrincipal(
+        "cognito-identity.amazonaws.com",
+        {
+          StringEquals: {
+            "cognito-identity.amazonaws.com:aud": this.identityPool.ref,
+          },
+          "ForAnyValue:StringLike": {
+            "cognito-identity.amazonaws.com:amr": "authenticated",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity"
+      ),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          "service-role/AWSLambdaBasicExecutionRole"
+        ),
+      ],
+    });
+
+    new cognito.CfnIdentityPoolRoleAttachment(
+      this,
+      "auth0-identitypool-role-attachment",
+      {
+        identityPoolId: this.identityPool.ref,
+        roles: {
+          authentiawscated: this.authenticatedUserGroupRole.roleArn,
+          unauthenticated: this.unauthenticatedUserGroupRole.roleArn,
+        },
       }
     );
 
