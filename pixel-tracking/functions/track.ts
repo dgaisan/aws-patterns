7fd23b923c37715442df6771c5d752d6e5e91067
dgaisan <dima.gaisan@gmail.com>
2024-03-24 14:30:26 -0700

    [pixel-tracking] adding debug info

--- pixel-tracking/functions/track.ts
+++ pixel-tracking/functions/track.ts
@@ -3,10 +3,12 @@ import { S3Event, Context } from "aws-lambda";
 
 
 export const handler: Handler = async (event: S3Event, context: Context) => {    
-    
+
   try {
     const bucketName = process.env.BUCKET_NAME;
     const trackingId = context.awsRequestId;
+
+    console.log("++++++++++++ Pixel Track handler");
     
   } catch (err) {
     console.log("ERROR", err);
